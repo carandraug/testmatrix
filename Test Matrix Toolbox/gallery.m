14afba87cd0bb1f71593839417c3d4341108c860
Carnë Draug <carandraug+dev@gmail.com>
2013-05-07 00:06:37 +0100

    Initial work on the gallery function

--- Test Matrix Toolbox/gallery.m
+++ Test Matrix Toolbox/gallery.m
@@ -1,65 +1,162 @@
-function [A, e] = gallery(n)
-%GALLERY    Famous, and not so famous, test matrices.
-%       A = GALLERY(N) is an N-by-N matrix with some special property.
-%       The following values of N are currently available:
-%         N = 3 is badly conditioned.
-%         N = 4 is the Wilson matrix.  Symmetric pos def, integer inverse.
-%         N = 5 is an interesting eigenvalue problem: defective, nilpotent.
-%         N = 8 is the Rosser matrix, a classic symmetric eigenvalue problem.
-%               [A, e] = GALLERY(8) returns the exact eigenvalues in e.
-%         N = 21 is Wilkinson's tridiagonal W21+, another eigenvalue problem.
+## Copyright (C) 1989-1995 Nicholas .J. Higham
+## Copyright (C) 2013 Carnë Draug
+##
+## This file is part of Octave.
+##
+## Octave is free software; you can redistribute it and/or modify it
+## under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 3 of the License, or (at
+## your option) any later version.
+##
+## Octave is distributed in the hope that it will be useful, but
+## WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+## General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with Octave; see the file COPYING.  If not, see
+## <http://www.gnu.org/licenses/>.
 
-%       Original version supplied with MATLAB.  Modified by N.J. Higham.
-%
-%       References:
-%       J.R. Westlake, A Handbook of Numerical Matrix Inversion and Solution
-%          of Linear Equations, John Wiley, New York, 1968.
-%       J.H. Wilkinson, The Algebraic Eigenvalue Problem, Oxford University
-%          Press, 1965.
+## -*- texinfo -*-
+## @deftypefn  {Function File} {} gallery (@var{name})
+## @deftypefnx {Function File} {} gallery (@var{name}, @var{args})
+## Create interesting matrices for testing.
+##
+## @end deftypefn
 
-if n == 3
-   A = [ -149   -50  -154
-          537   180   546
-          -27    -9   -25 ];
+## Code for the individual matrices by
+## Nicholas .J. Higham <Nicholas.J.Higham@manchester.ac.uk>
+## Adapted for Octave and into single gallery function by Carnë Draug
 
-elseif n == 4
-   A = [10     7     8     7
-         7     5     6     5
-         8     6    10     9
-         7     5     9    10];
+function [matrix, varargout] = gallery (name, varargin)
 
-elseif n == 5
-%   disp('Try to find the EXACT eigenvalues and eigenvectors.')
-%   Matrix devised by Cleve Moler.  Its Jordan form has just one block, with
-%   eigenvalue zero.  Proof: A^k is nonzero for k<5, zero for k=5.
-%   TRACE(A)=0.  No simple form for null vector.
-   A = [  -9     11    -21     63    -252
-          70    -69    141   -421    1684
-        -575    575  -1149   3451  -13801
-        3891  -3891   7782 -23345   93365
-        1024  -1024   2048  -6144   24572 ];
+  if (nargin < 1)
+    print_usage ();
+  elseif (! ischar (name))
+    error ("gallery: NAME must be a string.");
+  endif
 
-elseif n == 8
-   A  = [ 611.  196. -192.  407.   -8.  -52.  -49.   29.
-          196.  899.  113. -192.  -71.  -43.   -8.  -44.
-         -192.  113.  899.  196.   61.   49.    8.   52.
-          407. -192.  196.  611.    8.   44.   59.  -23.
-           -8.  -71.   61.    8.  411. -599.  208.  208.
-          -52.  -43.   49.   44. -599.  411.  208.  208.
-          -49.   -8.    8.   59.  208.  208.   99. -911.
-           29.  -44.   52.  -23.  208.  208. -911.   99.  ];
+  switch (tolower (name))
+    case "binomial"
+      error ("gallery: matrix %s not implemented.", name);
+    case "cauchy"
+      error ("gallery: matrix %s not implemented.", name);
+    case "chebspec"
+      error ("gallery: matrix %s not implemented.", name);
+    case "chebvand"
+      error ("gallery: matrix %s not implemented.", name);
+    case "chow"
+      error ("gallery: matrix %s not implemented.", name);
+    case "circul"
+      error ("gallery: matrix %s not implemented.", name);
+    case "clement"
+      error ("gallery: matrix %s not implemented.", name);
+    case "compar"
+      error ("gallery: matrix %s not implemented.", name);
+    case "condex"
+      error ("gallery: matrix %s not implemented.", name);
+    case "cycol"
+      error ("gallery: matrix %s not implemented.", name);
+    case "dorr"
+      error ("gallery: matrix %s not implemented.", name);
+    case "dramadah"
+      error ("gallery: matrix %s not implemented.", name);
+    case "fiedler"
+      error ("gallery: matrix %s not implemented.", name);
+    case "forsythe"
+      error ("gallery: matrix %s not implemented.", name);
+    case "frank"
+      error ("gallery: matrix %s not implemented.", name);
+    case "gearmat"
+      error ("gallery: matrix %s not implemented.", name);
+    case "gcdmat"
+      error ("gallery: matrix %s not implemented.", name);
+    case "grcar"
+      error ("gallery: matrix %s not implemented.", name);
+    case "hanowa"
+      error ("gallery: matrix %s not implemented.", name);
+    case "house"
+      error ("gallery: matrix %s not implemented.", name);
+    case "integerdata"
+      error ("gallery: matrix %s not implemented.", name);
+    case "invhess"
+      error ("gallery: matrix %s not implemented.", name);
+    case "invol"
+      error ("gallery: matrix %s not implemented.", name);
+    case "ipjfact"
+      error ("gallery: matrix %s not implemented.", name);
+    case "jordbloc"
+      error ("gallery: matrix %s not implemented.", name);
+    case "kahan"
+      error ("gallery: matrix %s not implemented.", name);
+    case "kms"
+      error ("gallery: matrix %s not implemented.", name);
+    case "krylov"
+      error ("gallery: matrix %s not implemented.", name);
+    case "lauchli"
+      error ("gallery: matrix %s not implemented.", name);
+    case "lehmer"
+      error ("gallery: matrix %s not implemented.", name);
+    case "leslie"
+      error ("gallery: matrix %s not implemented.", name);
+    case "lesp"
+      error ("gallery: matrix %s not implemented.", name);
+    case "lotkin"
+      error ("gallery: matrix %s not implemented.", name);
+    case "minij"
+      error ("gallery: matrix %s not implemented.", name);
+    case "moler"
+      error ("gallery: matrix %s not implemented.", name);
+    case "neumann"
+      error ("gallery: matrix %s not implemented.", name);
+    case "normaldata"
+      error ("gallery: matrix %s not implemented.", name);
+    case "orthog"
+      error ("gallery: matrix %s not implemented.", name);
+    case "parter"
+      error ("gallery: matrix %s not implemented.", name);
+    case "pei"
+      error ("gallery: matrix %s not implemented.", name);
+    case "poisson"
+      error ("gallery: matrix %s not implemented.", name);
+    case "prolate"
+      error ("gallery: matrix %s not implemented.", name);
+    case "randcolu"
+      error ("gallery: matrix %s not implemented.", name);
+    case "randcorr"
+      error ("gallery: matrix %s not implemented.", name);
+    case "randhess"
+      error ("gallery: matrix %s not implemented.", name);
+    case "randjorth"
+      error ("gallery: matrix %s not implemented.", name);
+    case "rando"
+      error ("gallery: matrix %s not implemented.", name);
+    case "randsvd"
+      error ("gallery: matrix %s not implemented.", name);
+    case "redheff"
+      error ("gallery: matrix %s not implemented.", name);
+    case "riemann"
+      error ("gallery: matrix %s not implemented.", name);
+    case "ris"
+      error ("gallery: matrix %s not implemented.", name);
+    case "sampling"
+      error ("gallery: matrix %s not implemented.", name);
+    case "smoke"
+      error ("gallery: matrix %s not implemented.", name);
+    case "toeppd"
+      error ("gallery: matrix %s not implemented.", name);
+    case "tridiag"
+      error ("gallery: matrix %s not implemented.", name);
+    case "triw"
+      error ("gallery: matrix %s not implemented.", name);
+    case "uniformdata"
+      error ("gallery: matrix %s not implemented.", name);
+    case "wathen"
+      error ("gallery: matrix %s not implemented.", name);
+    case "wilk"
+      error ("gallery: matrix %s not implemented.", name);
+    otherwise
+      error ("gallery: unknown matrix with NAME %s", name);
+  endswitch
 
-   %  Exact eigenvalues from Westlake (1968), p.150 (ei'vectors given too):
-   a = sqrt(10405); b = sqrt(26);
-   e = [-10*a,   0,   510-100*b,  1000,   1000,   510+100*b, ...
-        1020,   10*a]';
-
-elseif n == 21
-   % W21+, Wilkinson (1965), p.308.
-   E = diag(ones(n-1,1),1);
-   m = (n-1)/2;
-   A = diag(abs(-m:m)) + E + E';
-
-else
-   error('Sorry, that value of N is not available.')
-end
+endfunction
