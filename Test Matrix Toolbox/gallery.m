797d6c2660bbd03d567e09e08a516ca57193e8d5
Carnë Draug <carandraug+dev@gmail.com>
2013-05-07 00:08:07 +0100

    cauchy: implemented cauchy into gallery

--- Test Matrix Toolbox/gallery.m
+++ Test Matrix Toolbox/gallery.m
@@ -23,6 +23,22 @@
 ## Create interesting matrices for testing.
 ##
 ## @end deftypefn
+##
+## @deftypefn  {Function File} {@var{c} =} gallery ("cauchy", @var{x})
+## @deftypefnx {Function File} {@var{c} =} gallery ("cauchy", @var{x}, @var{y})
+## Create a Cauchy matrix.
+##
+## For vectors @var{x} and @var{y} of length N, creates an N-by-N
+## matrix with @code{@var{c}(i,j) = 1/(@var{x}(i)+@var{y}(j))}.
+## If unspecified, @var{y} defaults to @var{x}.  As a special case,
+## if @var{x} is a scalar, it is interpreted as @code{1:@var{x}}.
+##
+## Explicit formulas are known for @code{det (@var{c})} (which is nonzero
+## if @var{x} and @var{y} both have distinct elements) and the elements
+## of @code{inv (@var{c})}.  @var{c} is totally positive if
+## @code{0 < @var{x}(1) < @dots{} < @var{x}(N)} and
+## @code{0 < @var{y}(1) < @dots{} < @var{x}(N)}.
+## @end deftypefn
 
 ## Code for the individual matrices by
 ## Nicholas .J. Higham <Nicholas.J.Higham@manchester.ac.uk>
@@ -39,8 +55,7 @@ function [matrix, varargout] = gallery (name, varargin)
   switch (tolower (name))
     case "binomial"
       error ("gallery: matrix %s not implemented.", name);
-    case "cauchy"
-      error ("gallery: matrix %s not implemented.", name);
+    case "cauchy",      matrix = cauchy      (varargin{:});
     case "chebspec"
       error ("gallery: matrix %s not implemented.", name);
     case "chebvand"
@@ -159,4 +174,56 @@ function [matrix, varargout] = gallery (name, varargin)
       error ("gallery: unknown matrix with NAME %s", name);
   endswitch
 
+endfunction
+
+function C = cauchy (x, y)
+  ##CAUCHY  Cauchy matrix.
+  ##        C = CAUCHY(X, Y), where X, Y are N-vectors, is the N-by-N matrix
+  ##        with C(i,j) = 1/(X(i)+Y(j)).   By default, Y = X.
+  ##        Special case: if X is a scalar CAUCHY(X) is the same as CAUCHY(1:X).
+  ##        Explicit formulas are known for DET(C) (which is nonzero if X and Y
+  ##        both have distinct elements) and the elements of INV(C).
+  ##        C is totally positive if 0 < X(1) < ... < X(N) and
+  ##        0 < Y(1) < ... < Y(N).
+  ##
+  ##        References:
+  ##        N.J. Higham, Accuracy and Stability of Numerical Algorithms,
+  ##          Society for Industrial and Applied Mathematics, Philadelphia, PA,
+  ##          USA, 1996; sec. 26.1.
+  ##        D.E. Knuth, The Art of Computer Programming, Volume 1,
+  ##          Fundamental Algorithms, second edition, Addison-Wesley, Reading,
+  ##          Massachusetts, 1973, p. 36.
+  ##        E.E. Tyrtyshnikov, Cauchy-Toeplitz matrices and some applications,
+  ##          Linear Algebra and Appl., 149 (1991), pp. 1-18.
+  ##          O. Taussky and M. Marcus, Eigenvalues of finite matrices, in
+  ##          Survey of Numerical Analysis, J. Todd, ed., McGraw-Hill, New York,
+  ##          pp. 279-313, 1962. (States the totally positive property on p. 295.)
+
+  if (nargin < 1 || nargin > 2)
+    error ("gallery: 1 or 2 arguments are required for Cauchy matrix.");
+  elseif (! isnumeric (x))
+    error ("gallery: X must be numeric for Cauchy matrix.");
+  elseif (nargin == 2 && ! isnumeric (y))
+    error ("gallery: Y must be numeric for Cauchy matrix.");
+  endif
+
+  n = length (x);
+  if (n == 1)
+     n = x;
+     x = 1:n;
+  endif
+
+  if (nargin == 1)
+    y = x;
+  endif
+
+  ## Ensure x and y are column vectors
+  x = x(:);
+  y = y(:);
+  if (numel (x) != numel (y))
+    error ("gallery: X and Y must be vectors of same length for cauchy matrix.");
+  endif
+
+  C = x * ones (1, n) + ones (n, 1) * y.';
+  C = ones (n) ./ C;
 endfunction
