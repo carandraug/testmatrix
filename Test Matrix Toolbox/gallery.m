a747c9404941ccc2a83b5224072c195c2260991d
Carnë Draug <carandraug+dev@gmail.com>
2013-05-07 02:17:01 +0100

    clement: implemented clement into gallery

--- Test Matrix Toolbox/gallery.m
+++ Test Matrix Toolbox/gallery.m
@@ -105,6 +105,24 @@
 ## @code{W = [1 t t^2 @dots{} t^N]} is an eigenvalue of @var{c}, and
 ## @code{W(N:-1:1)} is an eigenvector of @var{c}.
 ## @end deftypefn
+##
+## @deftypefn  {Function File} {@var{c} =} gallery ("clement", @var{n})
+## @deftypefnx {Function File} {@var{c} =} gallery ("clement", @var{n}, @var{k})
+## Create a tridiagonal matrix with zero diagonal entries.
+##
+## @var{c} is a tridiagonal matrix with zero diagonal entries
+## and known eigenvalues.  It is singular if @var{n} is odd.  About 64
+## percent of the entries of the inverse are zero.  The eigenvalues
+## are plus and minus the numbers N-1, N-3, N-5, @dots{}, (1 or 0).
+##
+## For @var{k} = 0 (default) the matrix is unsymmetric, while for
+## @var{k} = 1 it is symmetric. @code{gallery ("clement", @var{n}, 1)} is
+## diagonally similar to @code{gallery ("clement", @var{n})}.
+##
+## Similar properties hold for @code{gallery ("tridiag", X, Y, Z)] where
+## @code{y = zeros (@var{n}, 1)}.  The eigenvalues still come in plus/minus
+## pairs but they are not known explicitly
+## @end deftypefn
 
 ## Code for the individual matrices by
 ## Nicholas .J. Higham <Nicholas.J.Higham@manchester.ac.uk>
@@ -126,8 +144,7 @@ function [matrix, varargout] = gallery (name, varargin)
     case "chebvand",    matrix = chebvand    (varargin{:});
     case "chow",        matrix = chow        (varargin{:});
     case "circul",      matrix = circul      (varargin{:});
-    case "clement"
-      error ("gallery: matrix %s not implemented.", name);
+    case "clement",     matrix = clement     (varargin{:});
     case "compar"
       error ("gallery: matrix %s not implemented.", name);
     case "condex"
@@ -454,6 +471,47 @@ function C = circul (v)
 
   v = v(:).';   # Make sure v is a row vector
   C = toeplitz ([v(1) v(n:-1:2)], v);
+endfunction
+
+function A = clement (n, k = 0)
+  ## CLEMENT   Clement matrix - tridiagonal with zero diagonal entries.
+  ##           CLEMENT(N, K) is a tridiagonal matrix with zero diagonal entries
+  ##           and known eigenvalues.  It is singular if N is odd.  About 64
+  ##           percent of the entries of the inverse are zero.  The eigenvalues
+  ##           are plus and minus the numbers N-1, N-3, N-5, ..., (1 or 0).
+  ##           For K = 0 (the default) the matrix is unsymmetric, while for
+  ##           K = 1 it is symmetric.
+  ##           CLEMENT(N, 1) is diagonally similar to CLEMENT(N).
+  ##
+  ##           Similar properties hold for TRIDIAG(X,Y,Z) where Y = ZEROS(N,1).
+  ##           The eigenvalues still come in plus/minus pairs but they are not
+  ##           known explicitly.
+  ##
+  ##           References:
+  ##           P.A. Clement, A class of triple-diagonal matrices for test
+  ##              purposes, SIAM Review, 1 (1959), pp. 50-52.
+  ##           A. Edelman and E. Kostlan, The road from Kac's matrix to Kac's
+  ##              random polynomials. In John~G. Lewis, editor, Proceedings of
+  ##              the Fifth SIAM Conference on Applied Linear Algebra Society
+  ##              for Industrial and Applied Mathematics, Philadelphia, 1994,
+  ##              pp. 503-507.
+  ##           O. Taussky and J. Todd, Another look at a matrix of Mark Kac,
+  ##              Linear Algebra and Appl., 150 (1991), pp. 341-360.
+
+  if (nargin < 1 || nargin > 2)
+    error ("gallery: 1 or 2 arguments are required for clement matrix.");
+  endif
+
+  n = n-1;
+  x = n:-1:1;
+  z = 1:n;
+
+  if (k == 0)
+     A = diag (x, -1) + diag (z, 1);
+  else
+     y = sqrt (x.*z);
+     A = diag (y, -1) + diag (y, 1);
+  endif
 endfunction
 
 
